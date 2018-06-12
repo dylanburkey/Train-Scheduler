cfe59db1106ea960f8f59e1da9924d0c22f40e69
dylanburkey <dylanburkey@outlook.com>
2018-06-12 13:47:33 -0400

    Add updated logic and styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,55 @@
-    // Initialize Firebase
- var config = {
+// Firebase Config String
+var config = {
     apiKey: "AIzaSyA7LH00IwXVyCoIcBmE-Q9rFWExzhPNhQw",
     authDomain: "train-scheduler-8d78e.firebaseapp.com",
     databaseURL: "https://train-scheduler-8d78e.firebaseio.com",
     projectId: "train-scheduler-8d78e",
     storageBucket: "train-scheduler-8d78e.appspot.com",
     messagingSenderId: "528165487668"
-  };i
+};
 
+// Pass config paramters to firebase
+firebase.initializeApp(config);
 
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-  var remainder = 0;
+// Set database to Firebase database.
+var database = firebase.database();
+  
+var remainder = 0;
   // var momentTime = moment(localTime, "hh:mm a").format("X");
   
-  var name = "";
-  var destination = "";
-  var time = 0;
-  var frequency = 0;
-  var nextArrival = 0;
-  var minutesAway = 0;
+// Train - Name
+var name = "";
+
+// Train - Destination
+var destination = "";
+
+// Train - Depart Time
+var time = 0;
+
+// Train - Frequency
+var frequency = 0;
+
+// Train - Next Arrival Time
+var nextArrival = 0;
+
+// Train - Current Minutes Away
+var minutesAway = 0;
   
-  // take your initial time, take your current time, find difference in minutes, divide(%) minutes by freq, take remainder and thats your time remaining 
-  // % 
+ // When a user clicks the "Add Train" button in our form
+ // pass values to firebase, when a new 
+
   
-  $("#add-user").on("click", function(event) {
+  $("#add-train").on("click", function(event) {
     event.preventDefault();
   
     // Grabbed values from text boxes
-    name        = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    time        = moment($("#time-input").val().trim(), "HH:mm").format("X");
-    frequency   = parseInt($("#frequency-input").val().trim());
+    name        = $("#train-name").val().trim();
+    destination = $("#train-destination").val().trim();
+    time        = moment($("#train-firstTime").val().trim(), "HH:mm").format("X");
+    frequency   = parseInt($("#train-frequency").val().trim());
     
   
-    // Code for handling the push
+    // Pass our values to Firebase using push
     database.ref().push({
       name: name,
       destination: destination,
@@ -47,7 +61,7 @@
   
   database.ref().on("child_added", function(childSnapshot) {
   
-          time        = childSnapshot.val().time;
+        time        = childSnapshot.val().time;
         frequency   = childSnapshot.val().frequency + 000;
         newTime = moment(moment.unix(time).add(frequency, "m")).format("X");
   
@@ -74,10 +88,6 @@
       // moment.unix(time).diff(moment(), "minutes")
         
   
-        console.log(childSnapshot.val().name);
-        console.log(childSnapshot.val().destination);
-        console.log(childSnapshot.val().time);
-        console.log(childSnapshot.val().frequency);
   
         $("#display").append("<tr><td>" + childSnapshot.val().name +
           "</td><td>" + childSnapshot.val().destination +
@@ -85,18 +95,17 @@
           "</td><td>" + nextArrival +
           "</td><td>" + minutesAway + " min" +"</td></tr>");
   
-        $("#name-input").val("");
-        $("#destination-input").val("");
-        $("#time-input").val("");
-        $("#frequency-input").val("");
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#train-firstTime").val("");
+        $("#train-frequencyt").val("");
   
       }, function(errorObject) {
         console.log("Errors handled: " + errorObject.code);
       });
 
-//
 
- // Do some stuff we want only only once.
-  ref.once("value", function(data) {
-    // do some stuff once
+/*  ref.once("value", function(data) {
+
   });
+  */
